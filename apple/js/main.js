e6fb9f30b096bdf15ed5df6f9bdeb2ea329444f6
CYBSGDSB <ccyb55@outlook.com>
2016-11-15 18:52:25 +0800

    2016/11/15

---
+++ apple/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/11/13.
+ */
+$(document).ready(function () {
+    $('#seach').click(function () {
+        console.log('a');
+
+        $('.t').addClass('an');
+    })
+})
+
